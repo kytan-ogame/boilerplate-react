d12d0a5989da2acd87b229341c544a40a288bfa6
julien.valery <julien.valery@manomano.com>
2023-06-05 10:50:31 +0200

    chore: adding husky and linting code

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,11 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
   ],
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['react', '@typescript-eslint', 'eslint-plugin-no-inline-styles'],
   rules: {
     'react/prop-types': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
@@ -22,7 +25,15 @@ module.exports = {
     'react/display-name': 'off',
     'no-console': 'error',
   },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
   settings: {
+    jest: {
+      version: 'latest',
+    },
     react: {
       version: 'detect',
     },
